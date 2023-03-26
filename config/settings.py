36075aa8847610036b8ffbb272c9f93d46490506
REZAD <rezadavoodi438@gmail.com>
2023-03-26 05:04:44 +0330

    crispy forms on login and signup

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # by RD
     'accounts',
     'pages',
@@ -130,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms setting
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
