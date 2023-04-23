03656835e9605140daa95ffeddfd3440ce6cabc4
REZAD <rezadavoodi438@gmail.com>
2023-04-23 17:54:32 +0330

    translate_numer renamed to translate_number

--- persian_translate/templatetags/persian_translation_tags.py
+++ persian_translate/templatetags/persian_translation_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.filter
-def translate_numer(value):
+def translate_number(value):
     value = str(value)
     english_to_persian_table = value.maketrans('0123456789', '۰۱۲۳۴۵۶۷۸۹')
     return value.translate(english_to_persian_table)
