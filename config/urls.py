ca0b9160d5af0636d73a12c50299c7d9bd934251
REZAD <rezadavoodi438@gmail.com>
2023-04-24 18:56:41 +0330

    order_create view and html without the form part was created

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
                   path('accounts/', include('allauth.urls')),
                   path('products/', include('products.urls')),
                   path('cart/', include('cart.urls')),
+                  path('order/', include('order.urls')),
 
                   # i18n rosetta
                   path('rosetta/', include('rosetta.urls'))
--- order/views.py
+++ order/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def order_create_view(reqeust):
+    return render(reqeust, 'order/order_create.html')
