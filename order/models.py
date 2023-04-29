2636d00e107aa635b483f32c3f3d68d5ecbfea0e
REZAD <rezadavoodi438@gmail.com>
2023-04-29 17:03:54 +0330

    order create form with django forms

--- order/models.py
+++ order/models.py
@@ -4,18 +4,18 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,verbose_name=_('User'))
+    is_paid = models.BooleanField(_('Is Paid?'), default=False)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
+    first_name = models.CharField(_('First Name'), max_length=100)
+    last_name = models.CharField(_('Last Name'), max_length=100)
+    phone_number = models.CharField(_('Phone Number'), max_length=100)
+    address = models.CharField(_('Address'), max_length=100)
 
-    order_notes = models.CharField(max_length=700, blank=True)
+    order_notes = models.CharField(_('Order Notes'), max_length=700, blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(_('Created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('Modified'), auto_now=True)
 
     def __str__(self):
         return f'Order number {self.id}'
@@ -23,7 +23,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name=_('items'))
-    product = models.ForeignKey('products.Product',on_delete=models.CASCADE, related_name=_('order_items'))
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name=_('order_items'))
     quantity = models.PositiveIntegerField(default=1)
     price = models.PositiveIntegerField()
 
--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .form import OrderForm
 
 def order_create_view(reqeust):
-    return render(reqeust, 'order/order_create.html')
+    return render(reqeust, 'order/order_create.html',context={'form':OrderForm})
