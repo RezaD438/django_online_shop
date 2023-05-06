ee9ed6d745cff561c700cb2819421427cd395559
REZAD <rezadavoodi438@gmail.com>
2023-05-06 18:34:53 +0330

    switched zarinpal sandbox

--- payment/views.py
+++ payment/views.py
@@ -95,5 +95,94 @@ def payment_callback(request):
                 return redirect('home')
 
     else:
-        messages.warning(request, 'payment unsuccessful')
+        messages.error(request, 'payment unsuccessful')
+        return redirect('home')
+
+
+def payment_process_sandbox(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
+    toman_total_price = order.get_total_price()
+    riyal_total_price = order.get_total_price() * 10
+
+    zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json'
+
+    request_header = {
+        "accept": "application/json",
+        "content-type": "application/json",
+    }
+
+    request_data = {
+        'MerchantID': settings.ZARINPAL_MERCHANT_ID,
+        'Amount': riyal_total_price,
+        'Description': f'#{order.id}: {order.user.first_name} {order.user.last_name}',
+        'CallbackURL': request.build_absolute_uri(reverse('payment_callback')),
+    }
+
+    res = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
+
+    data = res.json()
+    authority = data['Authority']
+    order.zarinpal_authority = authority
+    order.save()
+
+    if 'errors' not in data or len(data['errors']) == 0:
+        return redirect(f'https://sandbox.zarinpal.com/pg/StartPay/{authority}')
+    else:
+        messages.error(request, 'error from zarinpal')
+        return redirect('home')
+
+
+def payment_callback_sandbox(request):
+    payment_authority = request.GET.get('Authority')
+    payment_status = request.GET.get('Status')
+
+    order = get_object_or_404(Order, zarinpal_authority=payment_authority)
+
+    toman_total_price = order.get_total_price()
+    riyal_total_price = order.get_total_price() * 10
+
+    if payment_status == 'OK':
+
+        request_header = {
+            "accept": "application/json",
+            "content-type": "application/json",
+        }
+
+        request_data = {
+            'MerchantID': 'ABCDABCDABCDABCDABCDABCDABCDABCDABCD',
+            'Amount': riyal_total_price,
+            'Authority': payment_authority,
+        }
+
+        zarinpal_callback_url = 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentVerification.json'
+
+        res = requests.post(url=zarinpal_callback_url, data=json.dumps(request_data), headers=request_header)
+
+        if 'errors' not in res.json() or len(res.json()['errors']) == 0:
+            data = res.json()
+            payment_code = data['Status']
+
+            if payment_code == 100:
+                order.is_paid = True
+                order.ref_id = data['RefID']
+                order.zarinpal_data = data
+                order.save()
+
+                messages.success(request, 'payment successful')
+                return redirect('home')
+
+            elif payment_code == 101:
+                messages.warning(request, 'payment successful. but this payment was already registered.contact support')
+                return redirect('home')
+            else:
+                error_code = res.json()['errors']['code']
+                error_message = res.json()['errors']['message']
+                messages.error(request, 'there was an error on your payment')
+                messages.error(request, f'error message : {error_message} {error_code}')
+                return redirect('home')
+
+    else:
+        messages.error(request, 'payment unsuccessful')
         return redirect('home')
